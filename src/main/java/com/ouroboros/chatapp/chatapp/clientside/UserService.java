e982a89ac6246fcee4388c26ef30490819384f79
Bao Ngoc Duong <baongoc29110405@gmail.com>
2025-05-25 19:14:04 +0700

    Add LoginAndGetUser function

--- src/main/java/com/ouroboros/chatapp/chatapp/clientside/UserService.java
+++ src/main/java/com/ouroboros/chatapp/chatapp/clientside/UserService.java
@@ -1,26 +1,50 @@
 package com.ouroboros.chatapp.chatapp.clientside;
 
 import com.ouroboros.chatapp.chatapp.datatype.STATUS;
+import com.ouroboros.chatapp.chatapp.datatype.User;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 public class UserService {
-    private static final String SERVER_HOST = "localhost";
-    private static final int SERVER_PORT = 8080;
 
-    public static STATUS login(String email, String password) {
-        try (ClientConnection connection = new ClientConnection(SERVER_HOST, SERVER_PORT)) {
-            String response = connection.sendCommand("LOGIN " + email + " " + password);
-            return response.equals("SUCCESS") ? STATUS.SUCCESS : STATUS.FAILURE;
-        } catch (Exception e) {
+    public static User loginAndGetUser(String email, String password) {
+        try {
+            BufferedWriter out = ClientConnection.getSharedWriter();
+            BufferedReader in = ClientConnection.getSharedReader();
+
+            out.write("start: LOGIN\r\n");
+            out.write("email: " + email + "\r\n");
+            out.write("password: " + password + "\r\n");
+            out.write("end: LOGIN\r\n");
+            out.flush();
+
+            String response = in.readLine();
+            if ("SUCCESS".equals(response)) {
+                String username = in.readLine().split(": ")[1];
+                int userId = Integer.parseInt(in.readLine().split(": ")[1]);
+                return new User(userId, username, email);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
-            return STATUS.FAILURE;
         }
+        return null;
     }
 
     public static STATUS register(String username, String email, String password) {
-        try (ClientConnection connection = new ClientConnection(SERVER_HOST, SERVER_PORT)) {
-            String response = connection.sendCommand("REGISTER " + username + " " + email + " " + password);
-            return response.equals("SUCCESS") ? STATUS.SUCCESS : STATUS.FAILURE;
-        } catch (Exception e) {
+        try {
+            BufferedWriter out = ClientConnection.getSharedWriter();
+            out.write("start: REGISTER\r\n");
+            out.write("username: " + username + "\r\n");
+            out.write("email: " + email + "\r\n");
+            out.write("password: " + password + "\r\n");
+            out.write("end: REGISTER\r\n");
+            out.flush();
+
+            String response = ClientConnection.getSharedReader().readLine();
+            return "SUCCESS".equals(response) ? STATUS.SUCCESS : STATUS.FAILURE;
+        } catch (IOException e) {
             e.printStackTrace();
             return STATUS.FAILURE;
         }
