b288adab2ac57ae2e4f7928c9246ff24c6d1fc8e
Bao Ngoc Duong <baongoc29110405@gmail.com>
2025-05-26 14:54:21 +0700

    Update ServerBackend

--- src/main/java/com/ouroboros/chatapp/chatapp/serverside/ServerBackend.java
+++ src/main/java/com/ouroboros/chatapp/chatapp/serverside/ServerBackend.java
@@ -1,6 +1,8 @@
 package com.ouroboros.chatapp.chatapp.serverside;
 
 import com.ouroboros.chatapp.chatapp.datatype.Message;
+import com.ouroboros.chatapp.chatapp.datatype.STATUS;
+import com.ouroboros.chatapp.chatapp.datatype.User;
 import com.ouroboros.chatapp.chatapp.serverside.MessageHandler;
 import com.ouroboros.chatapp.chatapp.serverside.DatabaseUtils;
 
@@ -41,14 +43,124 @@ public class ServerBackend {
         try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
              BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
 
+            // Look for the start marker and determine the object type
             String line;
-            while ((line = in.readLine()) != null) {
-                if (line.equals("start: SEND_MESSAGE")) {
-                    System.out.println("Received SEND_MESSAGE request");
+            while (true) {
+                line = in.readLine();
+                if (line.equals("start: SEARCH_USERS_BY_NAME")) {
+                    System.out.println("[SERVER DEBUG] Received SEARCH_USERS_BY_NAME request");
+                    String query = "";
+                    while (!(line = in.readLine()).equals("end: SEARCH_USERS_BY_NAME")) {
+                        if (line.startsWith("query: ")) {
+                            query = line.substring("query: ".length());
+                        }
+                    }
+                    System.out.println("[SERVER DEBUG] SEARCH_USERS_BY_NAME query: " + query);
+                    PrintWriter pw = new PrintWriter(out, true);
+                    UserHandler.handleSearchUsersByName(query, pw);
+                } else if (line.equals("start: GET_ALL_USERS")) {
+                    System.out.println("[SERVER DEBUG] Received GET_ALL_USERS request");
+                    while (!(line = in.readLine()).equals("end: GET_ALL_USERS")) {
+                        // No parameters needed
+                    }
+                    PrintWriter pw = new PrintWriter(out, true);
+                    UserHandler.handleGetAllUsers(pw);
+                } else if (line.equals("start: CREATE_CHAT")) {
+                    ChatHandler.handleCreateChatRequest(in, out);
+                } else if (line.equals("start: GET_MESSAGES")) {
+                    int chatId = -1;
+                    while (!(line = in.readLine()).equals("end: GET_MESSAGES")) {
+                        if (line.startsWith("chatId: ")) {
+                            chatId = Integer.parseInt(line.substring("chatId: ".length()));
+                        }
+                    }
+                    System.out.println("Chat ID: " + chatId);
+                    if (chatId != -1) {
+                        MessageHandler.handleRequestMessages(chatId, out);
+                    }
+                } else if (line.equals("start: SEND_MESSAGE")) {
                     handleSendMessage(in, out);
+                } else if (line.equals("start: LOGIN")) {
+                    String email = null;
+                    String password = null;
+
+                    while (!(line = in.readLine()).equals("end: LOGIN")) {
+                        if (line.startsWith("email: ")) {
+                            email = line.substring("email: ".length());
+                        } else if (line.startsWith("password: ")) {
+                            password = line.substring("password: ".length());
+                        }
+                    }
+
+                    System.out.println("Login attempt for: " + email);
+
+                    User user = null;
+                    if (email != null && password != null) {
+                        user = AuthHandler.RequestLogin(email, password);
+                    }
+                    if (user != null) {
+                        System.out.println("Login successful for: " + email);
+                        out.write("start: AUTH_RESPONSE\r\n");
+                        out.write("status: SUCCESS\r\n");
+                        out.write("username: " + user.getUsername() + "\r\n");
+                        out.write("email: " + user.getEmail() + "\r\n");
+                        out.write("userId: " + user.getId() + "\r\n");
+                        out.write("end: AUTH_RESPONSE\r\n");
+                        out.flush();
+                    } else {
+                        System.out.println("Login failed for: " + email);
+                        out.write("start: AUTH_RESPONSE\r\n");
+                        out.write("status: FAILURE\r\n");
+                        out.write("end: AUTH_RESPONSE\r\n");
+                        out.flush();
+                    }
+                } else if (line.equals("start: REGISTER")) {
+                    String username = null;
+                    String email = null;
+                    String password = null;
+
+                    while (!(line = in.readLine()).equals("end: REGISTER")) {
+                        if (line.startsWith("username: ")) {
+                            username = line.substring("username: ".length());
+                        } else if (line.startsWith("email: ")) {
+                            email = line.substring("email: ".length());
+                        } else if (line.startsWith("password: ")) {
+                            password = line.substring("password: ".length());
+                        }
+                    }
+
+                    System.out.println("Registration attempt for: " + email);
+
+                    STATUS registerStatus = STATUS.FAILURE;
+
+                    if (username != null && email != null && password != null) {
+                        registerStatus = AuthHandler.RequestRegister(username, email, password);
+                    }
+                    if (registerStatus.equals(STATUS.SUCCESS)) {
+                        out.write("start: REGISTER_RESPONSE\r\n");
+                        out.write("status: SUCCESS\r\n");
+                        out.write("username: " + username + "\r\n");
+                        out.write("end: REGISTER_RESPONSE\r\n");
+                        out.flush();
+                    } else {
+                        out.write("start: REGISTER_RESPONSE\r\n");
+                        out.write("status: FAILURE\r\n");
+                        out.write("end: REGISTER_RESPONSE\r\n");
+                        out.flush();
+                    }
+                } else if (line.equals("start: LOGOUT")) {
+                    while (!(line = in.readLine()).equals("end: LOGOUT")) {
+                        // No parameters needed for logout
+                    }
+
+                    System.out.println("Logout request");
+                } else if (ChatHandler.isCreateChatRequest(line)) {
+                    ChatHandler.handleCreateChatRequest(in, out);
+                } else if (ChatHandler.isGetChatsRequest(line)) {
+                    ChatHandler.handleGetChatsRequest(in, out);
                 }
-                // Add other handlers here...
             }
+
         } catch (Exception e) {
             logger.log(Level.SEVERE, "Error handling client connection", e);
         } finally {
